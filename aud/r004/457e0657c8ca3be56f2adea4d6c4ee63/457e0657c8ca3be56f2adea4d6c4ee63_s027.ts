ccc35da8a0e0fb2dc44d843c6d77ff8da37500f6
vutiendat3601 <vutien.dat.3601@gmail.com>
2024-01-27 10:55:26 +0700

    aud: r004

---
+++ aud/r004/457e0657c8ca3be56f2adea4d6c4ee63/457e0657c8ca3be56f2adea4d6c4ee63_s027.ts
@@ -0,0 +1,11 @@
+ÕuÄ*í’IYˆlMf}*‡¼kÓ?Óh…( ™i‡«ºÚ³Úèf¸S…Åmóme³qˆ²®ð'l$‰r}„3+Eáu®>å®ždÊñ¥ê¡SI²yoäí-E&¡AO‹¾©ßVkk&wž"!SGƒøvhbÛ”±pÜ¥nŠú§R'—Ð©fÖ{Ø‹qï	fAwpÇfìóü¹ž:Mp?é”ÝßKšñà)‘Ý6Ý„ÇÚ/RöÚc~³Ê•»7D…ÄØ@;ú–»jŸË0_èî1ö¥Î¯¢Svn‰êqŠ²ñîµ¼6õËê¼ÿÞ7Å	¯ ¶ƒ~K¨9”LÍÅÌ.¼ÓI¦xñŽŸQAç8å·ZU•/JJã†\A `§JÐ¢dn—¤j-2ObÚgCëžƒ$8BWNO¬¦/ÆÇ³ÈÞÊ	ÃÊ&šL¿Šd¾|R–+ø5gE—Ê+`ô˜×\‚€¬§ôZsÐw”ô‘Êüp†±ÐðM2àR!3œ—%êÌ\Ÿ6õ¢wrODÞoÊW'QxÇÂÄ’¤üë!­/vº.´¥Oð°×.Ö
+z=ó~™ºÿðb;hËúnAƒÙJNYî0Ü’SYìÜ™ÖU2÷|O8+êtÿÓG~ø?m%m…ŠÇ†±sôÜˆ>Â¢'ðzFöºê8žS"zäÁ¨›Ic°æ3êœ)ÕŒkvðŽ¤„n&æLú,äj\âò‹Î0³ ªGò¶1LÎ–s`5;høl£â)l9°m¹
+…»ûïW7GQê¤z‡)	ùA97ý	ˆÃ+0“\aJËÆHŽ·'¦$È¸Ý‡Ç×3q[6%$ÿŒ¹ƒÎ(Ö0¶î@Lüµ &×þÿ¶˜$u©práìAa$ïÎ¬v•229¶–…è¸¯ÖsÞ©oX‹Š êÇÖØº5Üº)]IÏI$¯;û_Ô§ƒ$K7tG¢ª×ŒÇ¦»‡IÎ]øq9ì­îó²
+LÔL…›ŒÌBéz`{Qà±¿[­§J3V&ŒrÓêŒÕs?Pv6Å>ƒ¼P¢š…ÇöÈþ£gLëÒEg¤B¾‡AæVÌu¤7»‘½UmIŠXpí?ñ'é*/wcðÿgˆqä7¾–¼XNè#ûc~F«¥5a$M!{úù«FN†ÿåèÎ
+˜œFÌ»Ä¤œÚ£^×žôñÏ	·£2’l›0éöð*Ø‚‡‘O˜òeödÑ?z9Ê,Í<u[Q“i¼Ë÷I±s)]¦ÔLÞö¿ïjÂSÖ
+§Á‚ÃD&Sµ¦}‹^¯«÷OJgp!
+nâÖÉò”p,° #ƒ,¤§Í[Œ³Š}m´…[ž>þCChØ_Èé8¾RgeûìôžÓ7$qá
+Êˆ\+,'ž³ôWÇÎây2²Ù“AbÑHHú!y¨·)úC‰ö`O+Ä}&7pßØ2lšD.]^)Ç¯} µ´¦sVwÇÿJ¹ò>çóuˆœÃÚü_ÏRÂ=†cWµNPËà¬)€¸Ê½ ØËE±`ÆÁÀOêCóˆ'¹šÃ=H¶…y²v’øëâóy¯¹ÐOa¹–S]´Kúk‰àûKä~pAHŸ‰>ê:$;Y\ù¹é»jÏMÑªŠà>ÊF¤Ï»¦ z¤ÉFœ}ä°S•¸.pÙ÷Í+A‚ˆ/¼é9ñÿöŽ¾#î‹¨5‰kX1Ü/ÂN	¸¥ù‡ñr¼‘Ú¦ž~,ë•®„iÇ5x½Ç­Øò<òõÒÂv è0¯Ž3*×|Ñl;,;Œ×“ò•r˜òÌhÿKnî²6šxvô„Ó¾Ñá`â¿>¬áêØÛ¹ü“` Öž¶Ñuè¥«â™²&A"Ûè‚YóÅeÂŸ
+­[d³|
+nÈ¾
+¦ut .fþKtº	™ßßilÚI£€ÃÍk·«0¾Û&ËÜµˆä9Gõ¹ãnBŸ®Á_ìe§ÅÊÒQÔJ$û?Â#
